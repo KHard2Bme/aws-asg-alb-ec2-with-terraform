90c08301ac2321dd898bf05138f7e1d08b893950
Kevin Harding, MIS <146769989+KHard2Bme@users.noreply.github.com>
2025-09-14 23:21:18 -0400

    updates for Advanced

--- main.tf
+++ main.tf
@@ -178,7 +178,7 @@ resource "aws_lb_target_group" "app_tg" {
 
 resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.web_asg.id
-  alb_target_group_arn   = aws_lb_target_group.app_tg.arn
+  lb_target_group_arn   = aws_lb_target_group.app_tg.arn
 }
 
 resource "aws_lb_listener" "http_listener" {
