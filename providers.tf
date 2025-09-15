9dcd5423f24ae7370cf0f1a5cabdddfc083557bf
Kevin Harding, MIS <146769989+KHard2Bme@users.noreply.github.com>
2025-09-14 22:18:27 -0400

    update

--- providers.tf
+++ providers.tf
@@ -8,6 +8,16 @@ terraform {
   required_version = ">= 1.5.0"
 }
 
+terraform {
+# Configure remote state (update with your bucket details after creating it)
+  backend "s3" {
+    bucket        = "skybound-tfstate-5cad309b"
+    key           = "skybound-tfstate-5cad309b/backend/backend.tf"
+    region        = "us-east-1"
+    encrypt       =  true
+  }
+}
+
 
 provider "aws" {
   region = var.aws_region
