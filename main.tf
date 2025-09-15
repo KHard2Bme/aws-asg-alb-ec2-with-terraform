ddbe1e16a2f077c7b3f673b28a667611ede8b6c5
Kevin Harding, MIS <146769989+KHard2Bme@users.noreply.github.com>
2025-09-14 23:13:58 -0400

    updates for Advanced

--- main.tf
+++ main.tf
@@ -3,13 +3,11 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# Reference two existing subnets in the default VPC
 data "aws_subnet" "subnet_a" {
   filter {
     name   = "cidr-block"
     values = ["172.31.16.0/20"]
   }
-
   vpc_id = data.aws_vpc.default.id
 }
 
@@ -18,7 +16,6 @@ data "aws_subnet" "subnet_b" {
     name   = "cidr-block"
     values = ["172.31.64.0/20"]
   }
-
   vpc_id = data.aws_vpc.default.id
 }
 
@@ -30,30 +27,22 @@ locals {
 }
 
 
-# Security Group for Instances
-resource "aws_security_group" "web_sg" {
-  name        = "${var.project_name}-sg"
-  description = "Allow HTTP traffic from the internet"
+# Security Group for ALB
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project_name}-alb-sg"
+  description = "Allow inbound HTTP from the internet to ALB"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description = "Allow HTTP Traffic"
+    description = "HTTP from anywhere"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "Allow SSH Traffic"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.allowed_ssh_ip]
-  } 
-
   egress {
-    description = "Allow all outbound"
+    description = "All outbound"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -61,7 +50,42 @@ resource "aws_security_group" "web_sg" {
   }
 
   tags = {
-    Name = "${var.project_name}-sg"
+    Name = "${var.project_name}-alb-sg"
+  }
+}
+
+# Security Group for EC2 instances (only allow traffic from ALB)
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project_name}-ec2-sg"
+  description = "Allow HTTP traffic from ALB only"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description     = "HTTP from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    description = "Allow SSH (restricted)"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ssh_ip]
+  }
+
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-ec2-sg"
   }
 }
 
@@ -69,7 +93,7 @@ resource "aws_security_group" "web_sg" {
 # Get latest Amazon Linux AMI
 data "aws_ami" "al2023" {
   most_recent = true
-  owners      = ["137112412989"] # Amazon
+  owners      = ["137112412989"]
 
   filter {
     name   = "name"
@@ -119,12 +143,60 @@ resource "aws_autoscaling_group" "web_asg" {
   }
 }
 
+
+# ALB + Target Group + Listener
+resource "aws_lb" "app_lb" {
+  name               = "${var.project_name}-alb"
+  load_balancer_type = "application"
+  subnets            = local.selected_subnets
+  security_groups    = [aws_security_group.alb_sg.id]
+
+  tags = {
+    Name = "${var.project_name}-alb"
+  }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "${var.project_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "${var.project_name}-tg"
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.web_asg.id
+  alb_target_group_arn   = aws_lb_target_group.app_tg.arn
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
 # Random suffix for uniqueness
-resource "random_id" "suffix"  {
+resource "random_id" "suffix" {
   byte_length = 4
 }
 
-# S3 Bucket for Terraform Remote Backend
 resource "aws_s3_bucket" "tf_state" {
   bucket = "skybound-tfstate-${random_id.suffix.hex}"
 
@@ -141,7 +213,6 @@ resource "aws_s3_bucket_versioning" "tf_state" {
   }
 }
 
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
   bucket = aws_s3_bucket.tf_state.id
 
@@ -151,3 +222,5 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
     }
   }
 }
+
+
--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,5 @@
-# Get all EC2 instances in the ASG by tag
-data "aws_instances" "asg_instances" {
-  instance_tags = {
-    Name = "${var.project_name}-asg-instance"
-  }
-}
-
-# Public IPs of all instances in the Auto Scaling Group
-output "asg_instance_public_ips" {
-  description = "Public IP addresses of the Auto Scaling Group instances"
-  value       = data.aws_instances.asg_instances.public_ips
-}
-
-# IDs of all instances (helpful for debugging)
-output "asg_instance_ids" {
-  description = "Instance IDs of the Auto Scaling Group instances"
-  value       = data.aws_instances.asg_instances.ids
-}
-
-# S3 bucket name for Terraform remote backend
-output "s3_bucket_name" {
-  description = "S3 bucket created for Terraform remote backend"
-  value       = aws_s3_bucket.tf_state.bucket
+# Public DNS name for the ALB
+output "alb_dns_name" {
+  description = "Public DNS name of the Application Load Balancer"
+  value       = aws_lb.app_lb.dns_name
 }
